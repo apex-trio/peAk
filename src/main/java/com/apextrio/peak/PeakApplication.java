69dd1315105319100fd95f6e761de567389719a2
Nick Crain <nicholascrain@gmail.com>
2018-12-21 11:30:53 -0800

    initial commit

---
+++ src/main/java/com/apextrio/peak/PeakApplication.java
@@ -0,0 +1,14 @@
+package com.apextrio.peak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeakApplication.class, args);
+	}
+
+}
+
