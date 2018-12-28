5baa8a874ed79294aa4e785f65bb2dd819412ee1
Nick Crain <nicholascrain@gmail.com>
2018-12-28 11:44:53 -0800

    getters/setters for users

--- src/main/java/com/apextrio/peak/AppUser.java
+++ src/main/java/com/apextrio/peak/AppUser.java
@@ -15,5 +15,49 @@ public class AppUser {
     private String firstName;
     private String lastName;
     @ManyToMany(mappedBy = "users")
-    private Set<Team> groups = new HashSet<>();
+    public Set<Team> groups = new HashSet<>();
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getBio() {
+        return this.bio;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
