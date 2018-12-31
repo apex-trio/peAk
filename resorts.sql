dc284ab03b799352c20da1fa6769ce098e67001b
Nick Crain <nicholascrain@gmail.com>
2018-12-31 11:47:13 -0800

    sql file + templating for widget

---
+++ resorts.sql
@@ -0,0 +1,17 @@
+INSERT INTO resort (name, nick, widget_id, ots_url, website) VALUES ('Crystal Mountain Resort', 'Crystal', 124, 'https://www.onthesnow.com/washington/crystal-mountain-wa/skireport.html', 'http://crystalmountainresort.com/');
+
+INSERT INTO resort (name, nick, widget_id, ots_url, website) VALUES ('Vail Ski Resort', 'Vail', 482, 'https://www.onthesnow.com/colorado/vail/skireport.html','https://www.vail.com/');
+
+INSERT INTO resort (name, nick, widget_id, ots_url, website) VALUES ('Stevens Pass Resort', 427, 'https://www.onthesnow.com/washington/stevens-pass-resort/skireport.html','https://www.stevenspass.com/site');
+
+INSERT INTO resort (name, nick, widget_id, ots_url, website) VALUES ('Yosemite Ski & Snowboard Area', 'Yosemite', 29, 'https://www.onthesnow.com/california/badger-pass/skireport.html','https://www.yosemite.com/yosemite/yosemite-westbadger-pass/');
+
+INSERT INTO resort (name, nick, widget_id, ots_url, website) VALUES ('Mammoth Mountain Ski Area', 'Mammoth', 227, 'https://www.onthesnow.com/california/mammoth-mountain-ski-area/skireport.html','https://www.mammothmountain.com/winter/mountain-information/mountain-information');
+
+INSERT INTO resort (name, nick, widget_id, ots_url, website) VALUES ('Whistler Blackcomb', 'Whistler', 493, 'https://www.onthesnow.com/british-columbia/whistler-blackcomb/skireport.html','https://www.whistlerblackcomb.com/');
+
+INSERT INTO resort (name, nick, widget_id, ots_url, website) VALUES ('Park City Mountain Resort', 'Park City', 314, 'https://www.onthesnow.com/utah/park-city-mountain-resort/skireport.html','https://www.parkcitymountain.com/');
+
+INSERT INTO resort (name, nick, widget_id, ots_url, website) VALUES ('The Summit at Snoqualmie', 'Snoqualmie', 466, 'https://www.onthesnow.com/washington/the-summit-at-snoqualmie/skireport.html','https://summitatsnoqualmie.com/');
+
+INSERT INTO resort (name, nick, widget_id, ots_url, website) VALUES ('Mount Baker', 'Mt. Baker', 266, 'https://www.onthesnow.com/washington/mt-baker/skireport.html','https://www.mtbaker.us/');
