b4641ca136ab06a1d988ed58b7f18160d13724c6
Nick Crain <nicholascrain@gmail.com>
2019-01-01 14:45:07 -0800

    ajax request now re-renders all necessary resort info

--- src/main/resources/static/js/views.js
+++ src/main/resources/static/js/views.js
@@ -16,8 +16,19 @@ $(document).ready(() => {
            url:"/resorts/" + $(this)[0].dataset.id,
            type:"GET"
         }).done(resort => {
-            console.log(resort.name);
+            console.log(resort);
             $('#ajax_test').text(resort.name);
+            $('.ots-widget > iframe').attr('src', 'https://www.onthesnow.com/widget/snow?resort=' + resort.widgetId + '&color=b');
+            $('.ots-widget > p > a').attr('href', resort.otsUrl);
+            $('#teams').empty();
+            resort.teams.forEach(team => {
+                let div = $('<div class="team_card" data-id="' + team.id + '"></div>');
+                div.append('<span>' + team.difficulty + ' </span>');
+                div.append('<span>' + team.name + ' </span>');
+                div.append('<span>' + team.users.length + '/' + team.capacity + ' </span>');
+                div.append('<span>' + team.description + ' </span>');
+                $('#teams').append(div);
+            });
         })
 
     }
