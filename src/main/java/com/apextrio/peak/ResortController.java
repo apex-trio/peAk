fa26b36da7f9426c5a925008e8b80d57ce89c9b7
Nick Crain <nicholascrain@gmail.com>
2019-01-03 14:56:33 -0800

    added error handling for trying to add a team without being an authorized user

--- src/main/java/com/apextrio/peak/ResortController.java
+++ src/main/java/com/apextrio/peak/ResortController.java
@@ -2,10 +2,12 @@ package com.apextrio.peak;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -31,6 +33,11 @@ public class ResortController {
     //Route should be based on  a resorts id so the group can be assigned to the proper resort. Will take in name, capacity, and difficulty.
     @PostMapping("/resorts/{id}")
     public RedirectView createTeam(Team t, @PathVariable long id, @RequestParam String dateGoing, Principal p) {
+        if (p==null) {
+           RedirectView rv = new RedirectView("/error");
+           rv.setStatusCode(HttpStatus.FORBIDDEN);
+           return rv;
+        }
         AppUser user = (AppUser) (((UsernamePasswordAuthenticationToken) p).getPrincipal());
         user = userRepo.findById(user.getId()).get();
         Resort r = resortRepo.findById(id).get();
