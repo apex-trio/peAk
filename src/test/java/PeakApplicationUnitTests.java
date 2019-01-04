448f0671b8e2fe909e7a8b86b82bf9d71bcd6546
Darrin Howell <dbhowell09@gmail.com>
2019-01-03 17:40:05 -0800

    completed testing getters and setters for models

--- src/test/java/PeakApplicationUnitTests.java
+++ src/test/java/PeakApplicationUnitTests.java
@@ -1,10 +1,13 @@
 import com.apextrio.peak.AppUser;
+import com.apextrio.peak.Resort;
+import com.apextrio.peak.Team;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
 public class PeakApplicationUnitTests {
+
 
     @Test
     public void AppUserGettersAndSetters() {
@@ -29,7 +32,67 @@ public class PeakApplicationUnitTests {
 
         assertTrue("last name should be the same as what the getter gives us",
                 testUser.getLastName().equals("Dixon"));
+    }
 
+
+    @Test
+    public void ResortGettersAndSetters() {
+        Resort testResort = new Resort();
+        testResort.setName("Mission Ridge");
+        testResort.setNick("The Ridge");
+        testResort.setLatitude((float) 46.9233);
+        testResort.setLongitude((float) -121.476);
+        testResort.setOtsUrl("https://fakeOtsUrl.com");
+        testResort.setWebsite("https://skiResortWebsite.com");
+        testResort.setWidgetId(1);
+
+        assertTrue("full name of resort should be the same as what the getter gives us",
+                testResort.getName().equals("Mission Ridge"));
+
+        assertTrue("nickname of resort should be the same as what the getter gives us",
+                testResort.getNick().equals("The Ridge"));
+
+        assertEquals(testResort.getLatitude(), (float) 46.9233, 0);
+
+        assertEquals(testResort.getLongitude(), (float) -121.476, 0);
+
+
+        assertTrue("fake ots url should be the same as what the getter gives us",
+                testResort.getOtsUrl().equals("https://fakeOtsUrl.com"));
+
+        assertTrue("fake resort website should be the same as what the getter gives us",
+                testResort.getWebsite().equals("https://skiResortWebsite.com"));
+
+        assertEquals(testResort.getWidgetId(), 1, 0);
+    }
+
+
+    @Test
+    public void TeamGettersAndSetters() {
+        Team testTeam = new Team();
+        testTeam.setName("Squares aren't for squares");
+        testTeam.setDifficulty(1);
+        testTeam.setDescription("Casual riding");
+        testTeam.setCapacity(6);
+        testTeam.setDateCreated("01/5/2019");
+        testTeam.setDateGoing("01/19/2019");
+
+
+        assertTrue("group name should be the same as what the getter gives us",
+                testTeam.getName().equals("Squares aren't for squares"));
+
+        assertEquals(testTeam.getDifficulty(), 1, 0);
+
+        assertTrue("last name should be the same as what the getter gives us",
+                testTeam.getDescription().equals("Casual riding"));
+
+        assertEquals(testTeam.getCapacity(), 6, 0);
+
+        assertTrue("date created should be the same as what the getter gives us",
+                testTeam.getDateCreated().equals("01/5/2019"));
+
+        assertTrue("date going should be the same as what the getter gives us",
+                testTeam.getDateGoing().equals("01/19/2019"));
     }
 
 }
